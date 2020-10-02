8e17bcdb0b01533ad11c0604e5668574fd765950
lawalalao <lawalalaoad@gmail.com>
2020-10-02 17:18:57 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const bodyParser = require ('body-parser');
+const express = require('express');
+const mongoose = require ('mongoose');
+
+const stuffRoutes = require ('./routes/stuff');
+const userRoutes = require('./routes/user')
+
+
+
+mongoose.connect('mongodb+srv://lawal:Lawal@24+@cluster0.rmt0j.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+
+module.exports =app;
---
+++ routes/stuff.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
+
+module.exports = router;
